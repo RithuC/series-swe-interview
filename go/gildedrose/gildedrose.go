8e01e4138a491dd0a25f7ad0ba52672b8175069e
Rithu C <crithu@gmail.com>
2023-01-23 21:51:10 -0500

    add tests and conjured reqs with basic cleanup

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -5,54 +5,107 @@ type Item struct {
 	SellIn, Quality int
 }
 
-func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
+const (
+	AgedBrie        string = "Aged Brie"
+	Sulfuras        string = "Sulfuras, Hand of Ragnaros"
+	BackstagePasses string = "Backstage passes to a TAFKAL80ETC concert"
+	Conjured        string = "Conjured"
+)
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
+const (
+	MaxQuality          int = 50
+	MinQuality          int = 0
+	MaxQualityLegendary int = 80
+)
+
+func isSpecialProduct(name string) bool {
+	switch name {
+	case AgedBrie, Sulfuras, BackstagePasses, Conjured:
+		return true
+	}
+	return false
+}
+
+func UpdateQuality(items []*Item) {
+	for _, item := range items {
+
+		// Nothing changes for a legendary item
+		if item.Name == Sulfuras {
+			continue
+		}
+		//} else if !isSpecialProduct(item.Name) {
+		//	if item.Quality > MinQuality {
+		//		item.Quality--
+		//	}
+		//}
+		//
+		//if item.Name == Conjured && item.Quality > MinQuality {
+		//	item.Quality = item.Quality - 2
+		//}
+		//
+		//if item.Name == AgedBrie || item.Name == BackstagePasses {
+		//	if item.Quality < 50 {
+		//		item.Quality = item.Quality + 1
+		//		if item.Name == BackstagePasses {
+		//			if item.SellIn < 11 {
+		//				if item.Quality < 50 {
+		//					item.Quality = item.Quality + 1
+		//				}
+		//			}
+		//			if item.SellIn < 6 {
+		//				if item.Quality < 50 {
+		//					item.Quality = item.Quality + 1
+		//				}
+		//			}
+		//		}
+		//	}
+		//}
+
+		if item.Name != AgedBrie && item.Name != BackstagePasses {
+			if item.Quality > 0 {
+				item.Quality = item.Quality - 1
+				if item.Name == Conjured {
+					item.Quality = item.Quality - 1
 				}
 			}
 		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
+				if item.Name == BackstagePasses {
+					if item.SellIn < 11 {
+						if item.Quality < 50 {
+							item.Quality = item.Quality + 1
 						}
 					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
+					if item.SellIn < 6 {
+						if item.Quality < 50 {
+							item.Quality = item.Quality + 1
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
+		item.SellIn--
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
+		if item.SellIn < 0 {
+			if item.Name != AgedBrie {
+				if item.Name != BackstagePasses {
+					if item.Quality > 0 {
+						item.Quality = item.Quality - 1
 					}
 				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
+					item.Quality = item.Quality - item.Quality
 				}
 			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
+				if item.Quality < 50 {
+					item.Quality = item.Quality + 1
 				}
 			}
 		}
 	}
+}
 
+func updateSellIn(item *Item) {
+	item.SellIn--
 }
--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -1,19 +1,138 @@
-package gildedrose_test
+package gildedrose
 
 import (
 	"testing"
-
-	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 )
 
+/*
+- Once the sell by date has passed, Quality degrades twice as fast - DONE
+- The Quality of an item is never negative - DONE
+- "Aged Brie" actually increases in Quality the older it gets - DONE
+- The Quality of an item is never more than 50 - DONE
+- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality - DONE
+- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+ 			- Quality increases by 2 when there are 10 days or less and
+			- by 3 when there are 5 days or less but
+			- Quality drops to 0 after the concert
+*/
+
+//var items = []*Item{
+//	{"Standard", 5, 50},
+//	{"Past Sell Date", -1, 50},
+//	{"Quality Zero", 7, 0},
+//	{"Aged Brie", 7, 30},
+//	{"Aged Brie", 7, 50},                                  // Can't go over 50 Quality
+//	{"Aged Brie", -5, 10},                                 // Past sell date quality increase double
+//	{"Sulfuras, Hand of Ragnaros", 7, 80},                 // Doesn't change
+//	{"Sulfuras, Hand of Ragnaros", -10, 80},               // Doesn't change
+//	{"Backstage passes to a TAFKAL80ETC concert", 15, 10}, // Normal quality decrease
+//	{"Backstage passes to a BTS concert", 10, 10},         // Double quality increase
+//	{"Backstage passes to a DPR concert", 7, 10},          // Double quality increase
+//	{"Backstage passes to a DEAN concert", 5, 10},         // Triple quality increase
+//	{"Backstage passes to a Mitski concert", 3, 10},       // Triple quality increase
+//	{"Backstage passes to a Tabber concert", 1, 10},       // Triple quality increase
+//	{"Backstage passes to the Lumineers concert", -1, 50}, // Zero Quality
+//	{"Conjured", 5, 50},
+//	{"Conjured", 1, 0},
+//}
+
+//var expected = []*Item{
+//	{"Standard", 4, 49},
+//	{"Past Sell Date", -2, 48},
+//	{"Quality Zero", 6, 0},
+//	{"Aged Brie", 6, 31},
+//	{"Aged Brie", 6, 50},                                  // Can't go over 50 Quality
+//	{"Aged Brie", -6, 12},                                 // Past sell date quality increase double
+//	{"Sulfuras, Hand of Ragnaros", 7, 80},                 // Doesn't change
+//	{"Sulfuras, Hand of Ragnaros", -10, 80},               // Doesn't change
+//	{"Backstage passes to a TAFKAL80ETC concert", 14, 11}, // Normal quality decrease
+//	{"Backstage passes to a BTS concert", 9, 12},          // Double quality increase
+//	{"Backstage passes to a DPR concert", 6, 12},          // Double quality increase
+//	{"Backstage passes to a DEAN concert", 4, 13},         // Triple quality increase
+//	{"Backstage passes to a Mitski concert", 2, 13},       // Triple quality increase
+//	{"Backstage passes to a Tabber concert", 0, 13},       // Triple quality increase
+//	{"Backstage passes to the Lumineers concert", -2, 0},  // Zero Quality
+//	{"Conjured", 4, 48},
+//	{"Conjured", 0, 0},
+//}
+
 func Test_Foo(t *testing.T) {
-	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+	var tests = []struct {
+		name  string
+		input []*Item
+		want  []*Item
+	}{
+		{"Normal items",
+			[]*Item{{"Standard", 5, 50},
+				{"Past Sell Date", -1, 50},
+				{"Quality Zero", 7, 0}},
+			[]*Item{{"Standard", 4, 49},
+				{"Past Sell Date", -2, 48},
+				{"Quality Zero", 6, 0}}},
+
+		{"Aged Brie",
+			[]*Item{{"Aged Brie", 7, 30},
+				{"Aged Brie", 7, 50},
+				{"Aged Brie", -5, 10}},
+			[]*Item{{"Aged Brie", 6, 31},
+				{"Aged Brie", 6, 50},
+				{"Aged Brie", -6, 12}}},
+
+		{"Sulfuras",
+			[]*Item{{"Sulfuras, Hand of Ragnaros", 7, 80},
+				{"Sulfuras, Hand of Ragnaros", -10, 80}},
+			[]*Item{{"Sulfuras, Hand of Ragnaros", 7, 80},
+				{"Sulfuras, Hand of Ragnaros", -10, 80}}},
+
+		{"Backstage passes",
+			[]*Item{{"Backstage passes to a TAFKAL80ETC concert", 15, 10},
+				{"Backstage passes to a TAFKAL80ETC concert", 10, 10},
+				{"Backstage passes to a TAFKAL80ETC concert", 7, 10},
+				{"Backstage passes to a TAFKAL80ETC concert", 5, 10},
+				{"Backstage passes to a TAFKAL80ETC concert", 3, 10},
+				{"Backstage passes to a TAFKAL80ETC concert", 1, 10},
+				{"Backstage passes to a TAFKAL80ETC concert", -1, 50}},
+			[]*Item{{"Backstage passes to a TAFKAL80ETC concert", 14, 11},
+				{"Backstage passes to a TAFKAL80ETC concert", 9, 12},
+				{"Backstage passes to a TAFKAL80ETC concert", 6, 12},
+				{"Backstage passes to a TAFKAL80ETC concert", 4, 13},
+				{"Backstage passes to a TAFKAL80ETC concert", 2, 13},
+				{"Backstage passes to a TAFKAL80ETC concert", 0, 13},
+				{"Backstage passes to a TAFKAL80ETC concert", -2, 0}}},
+
+		{"Conjured",
+			[]*Item{{"Conjured", 5, 50},
+				{"Conjured", 1, 0}},
+			[]*Item{{"Conjured", 4, 48},
+				{"Conjured", 0, 0}}},
 	}
 
-	gildedrose.UpdateQuality(items)
+	//for i := 0; i < len(items); i++ {
+	//	if items[i].Name != expected[i].Name {
+	//		t.Errorf("Name: Expected %s but got %s ", expected[i].Name, items[i].Name)
+	//	}
+	//	if items[i].SellIn != expected[i].SellIn {
+	//		t.Errorf("Name: Expected %d but got %d ", expected[i].SellIn, items[i].SellIn)
+	//	}
+	//	if items[i].Quality != expected[i].Quality {
+	//		t.Errorf("Name: Expected %d but got %d ", expected[i].Quality, items[i].Quality)
+	//	}
+	//}
 
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	for _, test := range tests {
+		UpdateQuality(test.input)
+		for i := 0; i < len(test.input); i++ {
+			t.Run(test.name, func(t *testing.T) {
+				if test.input[i].Name != test.want[i].Name {
+					t.Errorf("Name: Expected %s but got %s ", test.want[i].Name, test.input[i].Name)
+				}
+				if test.input[i].SellIn != test.want[i].SellIn {
+					t.Errorf("SellIn: Expected %d but got %d ", test.want[i].SellIn, test.input[i].SellIn)
+				}
+				if test.input[i].Quality != test.want[i].Quality {
+					t.Errorf("Quality: Expected %d but got %d ", test.want[i].Quality, test.input[i].Quality)
+				}
+			})
+		}
 	}
 }
